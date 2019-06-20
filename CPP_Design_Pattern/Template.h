4efc6b8665c99d53e346d80d9911e404cbc3b5e9
jutheka <jutheka@gmail.com>
2019-06-20 08:28:55 -0400

    some clean up code from template design pattern

--- CPP_Design_Pattern/Template.h
+++ CPP_Design_Pattern/Template.h
@@ -32,19 +32,5 @@ public:
 
 
 
-class Base
-{
-	int x;
-public:
-	virtual void fun() = 0;
-	int getX();
-};
 
-// This class inherits from Base and implements fun() 
-class Derived : public Base
-{
-	int y;
-public:
-	void fun() { std::cout << "fun() called"; }
-};
 
